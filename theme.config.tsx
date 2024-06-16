753a7e15456ce78374044765de3d700bf8179377
Jon Wolf <172935675+jon-codewolf@users.noreply.github.com>
2024-06-16 18:40:39 +0200

    feat(about): init

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
+const logo = () => {
+  return (
+    <img src='/jon-codewolf.png' alt='Jon Wolf' width={36} height={36} />
+  )
+}
+
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/jon-codewolf/website',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/jon-codewolf/website',
   footer: {
     text: 'Nextra Docs Template',
   },
